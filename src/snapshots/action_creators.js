66c79993e8491ce1fafdccde3ae9126f13139a80
Zdenek Hynek <zdenek@signal-noise.co.uk>
2017-12-21 17:20:36 +0000

    Not send when the same url

--- src/snapshots/action_creators.js
+++ src/snapshots/action_creators.js
@@ -19,7 +19,23 @@ export function receiveCreateSnapshot(response) {
 
 export function createSnapshot(session, agent, title, url, sourceCode, image) {
   return (dispatch, getState) => {
-    const { auth } = getState();
+    const { auth, snapshots } = getState();
+
+    //  do not do anything if last snapshot had the same url and title
+    const lastSnapshot = snapshots.last();
+
+    if (lastSnapshot) {
+      if (
+        lastSnapshot.get('agent') === agent &&
+        lastSnapshot.get('title') === title &&
+        lastSnapshot.get('url') === url
+      ) {
+        console.log('Do not track, same page');
+        return;
+      }
+    }
+
+    console.log('Track, new page');
 
     dispatch(requestCreateSnapshot());
     dao.createSnapshot(session, agent, title, url, sourceCode, image,
