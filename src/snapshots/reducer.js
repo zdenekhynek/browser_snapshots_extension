fa315c231f20c6951dedd45bbba712c1d7303145
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-02-04 21:23:32 +0000

    Snapshot limit

--- src/snapshots/reducer.js
+++ src/snapshots/reducer.js
@@ -2,10 +2,23 @@ import { List, fromJS } from 'immutable';
 
 import { RECEIVE_CREATE_SNAPSHOT, CLEAR_SNAPSHOTS } from './action_creators';
 
+export const SNAPSHOT_LIMIT = 10;
+
+export function reduceCreateSnapshot(state, response) {
+  let newState = state.push(response);
+
+  //  there's a limit on messaging queu
+  if (newState.size > SNAPSHOT_LIMIT) {
+    newState = newState.shift();
+  }
+
+  return newState;
+}
+
 export default function(state = List(), action) {
   switch (action.type) {
     case RECEIVE_CREATE_SNAPSHOT:
-      return state.push(fromJS(action.response));
+      return reduceCreateSnapshot(state, fromJS(action.response));
     case CLEAR_SNAPSHOTS:
       return List();
     default:
