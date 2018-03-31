a895326e2a116889f97ec716a29657b48b7c09a6
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-03-31 14:09:11 +0100

    Only get relevant tasks for agent and status

--- src/tasks/dao.js
+++ src/tasks/dao.js
@@ -1,12 +1,13 @@
 import { daoFetch, formatUrl } from '../utils/dao_helpers';
 
 export function fetch(agent, token) {
+  //  always get queud up
+  const status = 1;
   const endpointUrl = 'tasks/';
 
   const method = 'GET';
-  const body = { agent };
   const options = { method };
 
-  const formattedUrl = formatUrl(endpointUrl);
+  const formattedUrl = formatUrl(endpointUrl, null, { agent, status });
   return daoFetch(formattedUrl, options, token);
 }
